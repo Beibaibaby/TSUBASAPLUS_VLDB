8d2d9a382a01eccd949803e800591efcb069f72d
PeizhenYang <83687784+PeizhenYang@users.noreply.github.com>
2022-11-11 08:46:04 -0500

    Update dataset_test.py

--- dataset_test.py
+++ dataset_test.py
@@ -9,12 +9,14 @@ import math
 
 size_bw = int(20) 
 ts=np.load('./datasets/data_noea.npy')#change to your local path
+#ts=ts[5:10,:]
 size_sliding=50
 
 
+thre=0.5
 mean_list, crosstime_stdmean =tsu.test_validity_check_time(ts,size_bw,size_sliding)
 #mean_list=mean_list[ (mean_list >= 50) & (mean_list <= -100) ]
-print(mean_list)
+print(crosstime_stdmean)
 #max_min_list=max_min_list[(max_min_list >= 50) & (max_min_list <= -100)]
 #p#rint(mean_list)
 plt.hist(mean_list,bins=100)
@@ -33,12 +35,12 @@ plt.show()
 
 print(np.mean(ts))
 
-ts=np.load('/localdisk2/draco/tsubasa/fin_dataset.npy')#change to your local path
+ts=np.load('./origindata.csv')#change to your local path
 size_sliding=50
 print(ts.shape)
 
 thre=0.5
-mean_list, max_min_list =tsu.test_validity(ts,size_bw,size_sliding)
+mean_list, crosstime_stdmean =tsu.test_validity(ts,size_bw,size_sliding)
 #p#rint(mean_list)
 plt.hist(mean_list,bins=100)
 plt.title("Hist of Mean of Std coeff of (each time series)-FIN")
@@ -47,7 +49,7 @@ plt.ylabel("Count")
 plt.savefig('plot_Hist of Mean of Std coeff of (each time series)_FIN')
 plt.show()
 
-plt.hist(max_min_list,bins=100)
+plt.hist(crosstime_stdmean,bins=100)
 plt.title("Hist of Mean of Max-Min of (each time series)-FIN")
 plt.xlabel("Mean of Max-Min of (each time series)")
 plt.ylabel("Count")
@@ -55,3 +57,4 @@ plt.savefig('plot_Hist of Mean of Max-Min of (each time series)_FIN')
 plt.show()
 
 print(np.mean(ts))
+
